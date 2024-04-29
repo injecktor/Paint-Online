c2701709f5142a9ee2d444f068736f77f6aa9ac2
InJeckt <p1o1l1i1e1x1o1v@mail.ru>
2024-04-29 14:10:06 +0500

    Added drawing by hover

--- Paint Online/windowProcedure.cpp
+++ Paint Online/windowProcedure.cpp
@@ -1,9 +1,11 @@
 #include "windowProcedure.h"
 
 PAINTSTRUCT ps;
-
 RECT rect;
 HBRUSH brush;
+
+RECT rect1;
+HBRUSH brush1;
 
 LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
@@ -12,18 +14,19 @@ LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
     case WM_CREATE:
         AddMenus(hWnd);
         MainAddWidgets(hWnd);
+        brush = CreateSolidBrush(RGB(255, 255, 255));
+        rect = CreateRect( 100, 100, 900, 500 );
+        rect1 = rect;
+        brush1 = brush;
         RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_VALIDATE);
         break;
     case WM_COMMAND:
         CommandHandler(hWnd, wp);
         break;
     case WM_PAINT:
-        brush = CreateSolidBrush(RGB(255, 255, 255));
-        rect = CreateRect( 100, 100, 900, 500 );
-
         BeginPaint(hWnd, &ps);
 
-        FillRect(ps.hdc, &rect, brush);
+        FillRect(ps.hdc, &rect1, brush1);
 
         EndPaint(hWnd, &ps);
         break;
@@ -34,6 +37,9 @@ LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 
         if (PtInRect(&rect, cursorPos)) {
             SetCursor(LoadCursorW(NULL, IDC_CROSS));
+            rect1 = CreateRect(cursorPos.x - 1, cursorPos.y - 1, 3, 3);
+            brush1 = CreateSolidBrush(RGB(0, 0, 0));
+            RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
         }
         else {
             SetCursor(LoadCursorW(NULL, IDC_ARROW));
