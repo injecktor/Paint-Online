4fdc7330dff19ba837fcee694e7c1f11276a5aca
InJeckt <p1o1l1i1e1x1o1v@mail.ru>
2024-04-29 21:07:39 +0500

    Fixed canvas borders for pen

--- Paint Online/windowProcedure.cpp
+++ Paint Online/windowProcedure.cpp
@@ -4,8 +4,12 @@ PAINTSTRUCT ps;
 RECT rect;
 HBRUSH brush;
 
+RECT rectRestrict;
+
 RECT rect1;
 HBRUSH brush1;
+
+int penWidth = 10;
 
 LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
@@ -20,6 +24,11 @@ LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
         rect1 = rect;
         brush1 = brush;
         RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_VALIDATE);
+        rectRestrict = rect;
+        rectRestrict.left += penWidth - penWidth / 2;
+        rectRestrict.top += penWidth - penWidth / 2;
+        rectRestrict.right -= penWidth - penWidth / 2 - 1;
+        rectRestrict.bottom -= penWidth - penWidth / 2 - 1;
         break;
     case WM_COMMAND:
         CommandHandler(hWnd, wp);
@@ -36,10 +45,10 @@ LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
         GetCursorPos(&cursorPos);
         ScreenToClient(hWnd, &cursorPos);
 
-        if (PtInRect(&rect, cursorPos)) {
+        if (PtInRect(&rectRestrict, cursorPos)) {
             SetCursor(LoadCursorW(NULL, IDC_CROSS));
             if (isDrawing) {
-                rect1 = CreateRect(cursorPos.x - 1, cursorPos.y - 1, 3, 3);
+                rect1 = CreateRect(cursorPos.x - penWidth / 2, cursorPos.y - penWidth / 2, penWidth, penWidth);
                 brush1 = CreateSolidBrush(RGB(0, 0, 0));
                 RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
             }
